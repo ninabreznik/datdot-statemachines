20580f09fc6cda028fc4e8b832c361bb7ffef1f2
ninabreznik <ninabreznik@gmail.com>
2021-03-11 03:41:57 +0000

    Update host state machine

--- src/datdot-service.js
+++ src/datdot-service.js
@@ -14,58 +14,29 @@ async function run_state_machine () {
 
   .onTransition(async (state) => {
     console.log('---------------------------------------------')
+    console.log(
+      'PARENT:', 
+      '| State:', state.value, 
+      '| Actors:', state.context.actors.map(actor => actor.name), 
+      '| Event:', state.event
+    ) 
 
-    console.log('PARENT:', '| State:', state.value, '| Context:', state.context, '| Event:', state.event)
-    console.log('~~~')
-    console.log('Children:')
-    // 'attest-0'
-    const child0 = state.context.actors.filter(el => el.name === 'attest-0')[0]
-    if (child0) {
-      const attest0 =child0.ref
-      console.log('attest-0', '| State:', attest0.state.value, '| Context:', attest0.state.context, '| event', attest0.state.event )
-      if (child0.ref.state.value === 'get_encoders_and_hosters_states' && !resolved) {
-        console.log('time to resolve')
-        datdot_service.send({ type: 'RESOLVE', id: '0', role: 'attest' })
-        resolved = true
-      }
-    }
-    // 'attest-01
-    const child1 = state.context.actors.filter(el => el.id === 'attest-1')[0]
-    if (child1) {
-      console.log('STATE for "attest-1" actor:', child1.ref.state.value, child1.ref.state.context )
-    }
-    
     await db.put('current_state', JSON.stringify(state))
   })
 
   .start()
 
   // ATTEST
-  datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'attest' })
+  // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'attest' })
   // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'attest' })
   // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'attest' })
   // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'attest' })
   // // ENCODE
-  // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'encode' })
+  datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'encode' })
   // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'encode' })
   // // HOST
   // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'host' })
   // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'host' })
-
-  // datdot_service.send({ type: 'REJECT', id: '1', role: 'attest' })
-  // datdot_service.send({ type: '_RETRY', id: '1', role: 'attest' })
-  // datdot_service.send({ type: 'REJECT', id: '1', role: 'attest' })
-  // datdot_service.send({ type: '_RETRY', id: '1', role: 'attest' })
-  // datdot_service.send({ type: 'RESOLVE', id: '1', role: 'attest' })
-  // datdot_service.send({ type: 'RESOLVE', id: '1', role: 'attest' })
-  // datdot_service.send({ type: 'RESOLVE', id: '1', role: 'attest' })
-  // datdot_service.send({ type: 'RESOLVE', id: '1', role: 'attest' })
-
-  // datdot_service.send({ type: 'RESOLVE', id: '2', role: 'attest' })
-  // datdot_service.send({ type: 'REJECT', id: '2', role: 'attest' })
-  // datdot_service.send({ type: 'RESOLVE', id: '2', role: 'attest' })
-  // datdot_service.send({ type: 'RESOLVE', id: '2', role: 'attest' })
-
 }
 
 run_state_machine()

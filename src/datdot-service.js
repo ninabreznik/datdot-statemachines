eb5a2676a2ef95f49352c6d7e634c9660e42171c
ninabreznik <ninabreznik@gmail.com>
2021-03-12 04:59:59 +0000

    Update datdot-service.js

--- src/datdot-service.js
+++ src/datdot-service.js
@@ -4,39 +4,77 @@ const datdot_service_SM = require('datdot-service-sm')
 const make_db = require('make-hyperbee')
 
 async function run_state_machine () {
-  var id = 0
   const db = await make_db()
-  const last_saved = await db.get('current_state')
-  if (last_saved) console.log('LAST SAVED STATE:', JSON.parse(last_saved.value).value)
-  var resolved = false
 
-  const datdot_service = interpret(datdot_service_SM)
-
+  const service = interpret(datdot_service_SM)
   .onTransition(async (state) => {
     console.log('---------------------------------------------')
     console.log(
       'PARENT:', 
       '| State:', state.value, 
-      '| Actors:', state.context.actors.map(actor => actor.name), 
+      '| Actors:', state.context.actors ? state.context.actors.map(actor => actor.name) : '',
       '| Event:', state.event
-    ) 
+    )
 
-    await db.put('current_state', JSON.stringify(state))
   })
-
   .start()
 
-  // ATTEST
-  // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'attest' })
-  // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'attest' })
-  // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'attest' })
-  // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'attest' })
-  // // ENCODE
-  datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'encode' })
-  // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'encode' })
-  // // HOST
-  // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'host' })
-  // datdot_service.send({ type: 'HOSTING_SETUP', id: id++, role: 'host' })
+  const hosting_setup = async (id, contract_id, role) => {
+    const name = await get_name(id, contract_id, role)
+    service.send({ type: 'HOSTING_SETUP', name })
+    if (name.includes('host-setup')) {
+      await db.put(`${id}/${contract_id}`, name)
+    }
+  }
+  const performance_check = async (id, contract_id, role) =>  {
+    const name = await get_name(id, contract_id, role)
+    service.send({ type: 'PERFORMANCE_CHECK', name })
+  }
+  const storage_proof = async (id, contract_id, role) => {
+    const name = await get_name(id, contract_id, role)
+    service.send({ type: 'STORAGE_PROOF', name })
+  }
+  const hosting_repair = async (id, contract_id, role) => {
+    const name = await get_name(id, contract_id, role)
+    service.send({ type: 'HOSTING_REPAIR', name })
+  }
+  const hosting_pause = async (id, contract_id, role) => {
+    const name = await get_name(id, contract_id, role)
+    service.send({ type: 'HOSTING_PAUSE', name })
+  }
+  const hosting_resume = async (id, contract_id, role) => {
+    const name = await get_name(id, contract_id, role)
+    service.send({ type: 'HOSTING_RESUME', name })
+  }
+  const hosting_end = async (id, contract_id, role) => {
+    const name = await get_name(id, contract_id, role)
+    service.send({ type: 'HOSTING_END', name })
+  }
+
+  async function get_name (id, contract_id, role) {
+    console.log('KEY', `${id}/${contract_id}`)
+    var buff = await db.get(`${id}/${contract_id}`)
+    if (buff) {
+      var name = buff.value.toString('utf-8')
+      console.log({name}, {role})
+      return name
+    }
+    else {
+      const prefix = random_int(1000, 10000)
+      return `${prefix}-${role}-${id}`
+    }
+  }
+
+  function random_int(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+  // scenario
+  // hosting_setup(11, 246, 'attest-setup')
+  hosting_setup(26, 246, 'host-setup')
+  // hosting_setup(17, 246, 'encode')
+  setTimeout(() => storage_proof(26, 246, 'host-storage'), 5000)
+
 }
 
 run_state_machine()
